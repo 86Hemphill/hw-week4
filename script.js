d5c4e8abde240d12e724227e7a99248a7ff84bb5
Logan Hemphill <logan.hemphill86@gmail.com>
2020-03-26 17:54:14 -0400

    function v1.2

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 // Create Variables
-var highScore = document.getElementById("highScorez");
+var highScore = document.getElementById("highScore");
+var highScoreContent = highScore.innerHTML;
 var timer = document.getElementById("timer");
 var stQuiz = document.getElementById("btn");
 var jb = document.getElementById("jumbotron");
 var oList = document.getElementById("list");
 var quest = document.getElementById("quest");
+
 
 
 
@@ -27,25 +29,82 @@ var q2A = [
     createListItem("img"),
     createListItem("div")
 ];
-var q3A = createListItem(["br", "lb", "bk", "linb"]);
-var q4A = createListItem(["head", "body", "section", "article"]);
+var q3A = [
+    createListItem("br"),
+    createListItem("lb"),
+    createListItem("bk"),
+    createListItem("linb")
+];
+var q4A = [
+    createListItem("head"),
+    createListItem("body"),
+    createListItem("section"),
+    createListItem("article")
+];
 
 // Event listener
-stQuiz.addEventListener("click", function () {
+// stQuiz.addEventListener("click", function () {
+//     var highScoreContent = 0;
+//     runQuiz(oList, q1A)
+//     stQuiz.style.display = "none";
+//     questContent = document.createTextNode(questions[0]);
+//     quest.appendChild(questContent);
+//         if (q1A[0].addEventListener("click", function () {
+//             questContent.remove();
+//             $(oList).empty();
+//             runQuiz(oList, q2A)
+//             stQuiz.style.display = "none";
+//             questContent = document.createTextNode(questions[1]);
+//             quest.appendChild(questContent);
+//             highScoreContent += 10;
+        
+//         )} else if (q1A[1].addEventListener("click", function () {
+//                 (q1A[1].addEventListener("click", function () {
+//                 questContent.remove();
+//                 $(oList).empty();
+//                 runQuiz(oList, q2A)
+//                 stQuiz.style.display = "none";
+//                 questContent = document.createTextNode(questions[1]);
+//                 quest.appendChild(questContent);
+        
+//     }
+// });
+
+stQuiz.addEventListener("click", function() {
+    var highScoreContent = 0;
     runQuiz(oList, q1A)
     stQuiz.style.display = "none";
     questContent = document.createTextNode(questions[0]);
     quest.appendChild(questContent);
+    if (q1A[0].addEventListener("click", function () {
+        questContent.remove();
+        $(oList).empty();
+        runQuiz(oList, q2A)
+        stQuiz.style.display = "none";
+        questContent = document.createTextNode(questions[1]);
+        quest.appendChild(questContent);
+        highScoreContent += 10; 
+        
+    })) {} else if(q1A[1||2||3].addEventListener("click", function (){
+        questContent.remove();
+        $(oList).empty();
+        runQuiz(oList, q2A)
+        stQuiz.style.display = "none";
+        questContent = document.createTextNode(questions[1]);
+        quest.appendChild(questContent);
+        highScoreContent += 10; 
+        
+    })) {}
 });
 
-q1A[0].addEventListener("click", function () {
-    questContent.remove();
-    $(oList).empty();
-    runQuiz(oList, q2A)
-    stQuiz.style.display = "none";
-    questContent = document.createTextNode(questions[1]);
-    quest.appendChild(questContent);
-});
+// q1A[0].addEventListener("click", function () {
+//     questContent.remove();
+//     $(oList).empty();
+//     runQuiz(oList, q2A)
+//     stQuiz.style.display = "none";
+//     questContent = document.createTextNode(questions[1]);
+//     quest.appendChild(questContent);
+// });
 
 
 //  Create <li>
