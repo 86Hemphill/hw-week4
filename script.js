8577f343bed88c8ab6c4b7d0119486afd0254b68
Logan Hemphill <logan.hemphill86@gmail.com>
2020-03-28 14:34:04 -0400

    function v1.3

--- script.js
+++ script.js
@@ -7,123 +7,87 @@ var jb = document.getElementById("jumbotron");
 var oList = document.getElementById("list");
 var quest = document.getElementById("quest");
 
-
-
-
-
 // Q&A's
-var questions = [
-    "Which component is not considered a primary building block of web dev?",
-    "Which of the following is not an html tag?",
-    "Which tag creates a line break?",
-    "Which section should the page title be placed in?"];
-var q1A = [
-    createListItem("Snake"),
-    createListItem("html"),
-    createListItem("css"),
-    createListItem("javascript")
-];
-var q2A = [
-    createListItem("gig"),
+
+var q1 = {
+    question : "Which component is not considered a primary building block of web dev?",
+    options : [
+        createListItem("Snake"),
+        createListItem("html"),
+        createListItem("css"),
+        createListItem("javascript")],
+    correctAnswer : createListItem("Snake"),
+    wrongAnswer : [
+        createListItem("html"),
+        createListItem("css"),
+        createListItem("javascript")]
+    };
+var q2 = {
+    question : "Which of the following is not an html tag?",
+    options : [createListItem("gig"),
     createListItem("p"),
     createListItem("img"),
-    createListItem("div")
-];
-var q3A = [
-    createListItem("br"),
-    createListItem("lb"),
+    createListItem("div")],
+    correctAnswer : createListItem("gig"),
+    wrongAnswer : [createListItem("p"),
+    createListItem("img"),
+    createListItem("div")]
+    };
+var q3 = [{
+    question : "Which tag creates a line break?",
+    correctAnswer : createListItem("br"),
+    wrongAnswer : [createListItem("lb"),
     createListItem("bk"),
-    createListItem("linb")
-];
-var q4A = [
-    createListItem("head"),
-    createListItem("body"),
+    createListItem("linb")]
+    }];
+var q4 = [{
+    question : "Which section should the page title be placed in?",
+    correctAnswer : createListItem("head"),
+    wrongAnswer : [createListItem("body"),
     createListItem("section"),
-    createListItem("article")
-];
+    createListItem("article")]
+    }];
 
 // Event listener
-// stQuiz.addEventListener("click", function () {
-//     var highScoreContent = 0;
-//     runQuiz(oList, q1A)
-//     stQuiz.style.display = "none";
-//     questContent = document.createTextNode(questions[0]);
-//     quest.appendChild(questContent);
-//         if (q1A[0].addEventListener("click", function () {
-//             questContent.remove();
-//             $(oList).empty();
-//             runQuiz(oList, q2A)
-//             stQuiz.style.display = "none";
-//             questContent = document.createTextNode(questions[1]);
-//             quest.appendChild(questContent);
-//             highScoreContent += 10;
-        
-//         )} else if (q1A[1].addEventListener("click", function () {
-//                 (q1A[1].addEventListener("click", function () {
-//                 questContent.remove();
-//                 $(oList).empty();
-//                 runQuiz(oList, q2A)
-//                 stQuiz.style.display = "none";
-//                 questContent = document.createTextNode(questions[1]);
-//                 quest.appendChild(questContent);
-        
-//     }
-// });
 
 stQuiz.addEventListener("click", function() {
     var highScoreContent = 0;
-    runQuiz(oList, q1A)
+    runQuiz (oList, q1.options);
     stQuiz.style.display = "none";
-    questContent = document.createTextNode(questions[0]);
+    questContent = document.createTextNode(q1.question);
     quest.appendChild(questContent);
-    if (q1A[0].addEventListener("click", function () {
-        questContent.remove();
-        $(oList).empty();
-        runQuiz(oList, q2A)
-        stQuiz.style.display = "none";
-        questContent = document.createTextNode(questions[1]);
-        quest.appendChild(questContent);
-        highScoreContent += 10; 
-        
-    })) {} else if(q1A[1||2||3].addEventListener("click", function (){
-        questContent.remove();
-        $(oList).empty();
-        runQuiz(oList, q2A)
-        stQuiz.style.display = "none";
-        questContent = document.createTextNode(questions[1]);
-        quest.appendChild(questContent);
-        highScoreContent += 10; 
-        
-    })) {}
 });
-
-// q1A[0].addEventListener("click", function () {
-//     questContent.remove();
-//     $(oList).empty();
-//     runQuiz(oList, q2A)
-//     stQuiz.style.display = "none";
-//     questContent = document.createTextNode(questions[1]);
-//     quest.appendChild(questContent);
-// });
-
-
-//  Create <li>
+// //  Create <li>
 
 function createListItem (text) {
     listItem = document.createElement("li");
-    listItem.textContent = text;
+    listItem.textContent = (text);
     return listItem;
-}
+};
 
 function runQuiz (parent, children) {
     children.forEach(function (child) {
         parent.appendChild(child);
-    });
-};
+})};
 
+q1.correctAnswer.addEventListener("click", function () {
+    questContent.remove();
+    $(oList).empty();
+    runQuiz(oList, q2.options);
+    stQuiz.style.display = "none";
+    questContent = document.createTextNode(q2.question);
+    quest.appendChild(questContent);
+    highScoreContent += 10; 
+});   
 
-
-
-
-
-
+q1.wrongAnswer.forEach(function(element) {
+    element.addEventListener("click", function () {
+    questContent.remove();
+    $(oList).empty();
+    runQuiz(oList, q2.options);
+    stQuiz.style.display = "none";
+    questContent = document.createTextNode(q2.options);
+    quest.appendChild(questContent);
+    highScoreContent += 10; 
+})});    
+   
